e031290af22357e6acc4d569fecbd85648dfd38e
denis.bilyk <denis.bilyk@bonial.com>
2015-09-13 16:21:58 +0300

    dht11.cpp refactoring

--- DHT11/dht11.cpp
+++ DHT11/dht11.cpp
@@ -18,36 +18,25 @@ uint8_t dht11_getdata(uint8_t select) {
     memset(bits, 0, sizeof(bits));
 
     //reset port
-    //DHT11_DDR |= (1 << DHT11_INPUTPIN); //output
-    //DHT11_PORT |= (1 << DHT11_INPUTPIN); //high
     pinMode(DHT11, OUTPUT);
     digitalWrite(DHT11, HIGH);
     _delay_ms(100);
 
     //send request
-    //DHT11_PORT &= ~(1 << DHT11_INPUTPIN); //low
     digitalWrite(DHT11, LOW);
     _delay_ms(18);
-    //DHT11_PORT |= (1 << DHT11_INPUTPIN); //high
     digitalWrite(DHT11, HIGH);
     _delay_us(1);
-    //DHT11_DDR &= ~(1 << DHT11_INPUTPIN); //input
     pinMode(DHT11, INPUT);
     _delay_us(39);
 
     //check start condition 1
-    /*if ((DHT11_PIN & (1 << DHT11_INPUTPIN))) {
-        return DHT11_ERROR;
-    }*/
     if (digitalRead(DHT11))
         return DHT11_ERROR;
 
 
     _delay_us(80);
     //check start condition 2
-    /* if (!(DHT11_PIN & (1 << DHT11_INPUTPIN))) {
-         return DHT11_ERROR;
-     }*/
     if (!digitalRead(DHT11))
         return DHT11_ERROR;
     _delay_us(80);
@@ -56,22 +45,17 @@ uint8_t dht11_getdata(uint8_t select) {
     for (j = 0; j < 5; j++) { //read 5 byte
         uint8_t result = 0;
         for (i = 0; i < 8; i++) {//read every bit
-//            while (!(DHT11_PIN & (1 << DHT11_INPUTPIN))); //wait for an high input
             while (!digitalRead(DHT11));
             _delay_us(30);
-//            if (DHT11_PIN & (1 << DHT11_INPUTPIN)) //if input is high after 30 us, get result
             if (digitalRead(DHT11))
                 result |= (1 << (7 - i));
-//            while (DHT11_PIN & (1 << DHT11_INPUTPIN)); //wait until input get low
             while (digitalRead(DHT11));
         }
         bits[j] = result;
     }
 
     //reset port
-    //  DHT11_DDR |= (1 << DHT11_INPUTPIN); //output
     pinMode(DHT11, OUTPUT);
-    // DHT11_PORT |= (1 << DHT11_INPUTPIN); //low
     digitalWrite(DHT11, HIGH);
     _delay_ms(100);
 
